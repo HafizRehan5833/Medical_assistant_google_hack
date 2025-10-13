caf7bc729109c596c1cb5d4515bb95586eaa4e1c
HafizRehan5833 <184770700+HafizRehan5833@users.noreply.github.com>
2025-10-13 22:03:14 +0500

    Google Hackathon

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
-from fastapi.middleware.cors import CORSMiddleware # type: ignore
-import os
-import base64
-import io
-from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware # type: ignore
+# import os
+# import base64
+# import io
+# from fastapi import FastAPI
 
 
 
-from routes import route
-from Authentications import auth_routes
+# from routes import route
+# from Authentications import auth_routes
 
 
-# FastAPI app
-app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# # FastAPI app
+# app = FastAPI()
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 
 
-app.include_router(route.chat_router, prefix="/chat", tags=["Chat"])    
-app.include_router(auth_routes.Auth_router, prefix="/auth", tags=["Auth"])
+# app.include_router(route.chat_router, prefix="/chat", tags=["Chat"])    
+# app.include_router(auth_routes.Auth_router, prefix="/auth", tags=["Auth"])
 
